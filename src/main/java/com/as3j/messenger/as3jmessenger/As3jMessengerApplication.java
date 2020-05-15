621c141c5a5f17d4167cf0d87ea3e6b960ca6200
Jakub Zygmunt <216938@edu.p.lodz.pl>
2020-05-15 12:45:55 +0200

    add(project): init

---
+++ src/main/java/com/as3j/messenger/as3jmessenger/As3jMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.as3j.messenger.as3jmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class As3jMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(As3jMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/as3j/messenger/as3jmessenger/As3jMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as3j.messenger.as3jmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class As3jMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
