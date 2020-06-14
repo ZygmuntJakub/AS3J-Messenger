f864d8dac81f7ba5dd237645d60fe535b206a240
Jakub Flaszka <56125923+FlaszKub@users.noreply.github.com>
2020-06-14 23:37:08 +0200

    Feature/register (#10)
    * Dto for register.
    
    * Tests for register user.
    
    * registration done
    
    * hoot fixes
    
    Co-authored-by: 4kaze <32jacke32@gmail.com>

--- src/main/java/com/as3j/messenger/controllers/UserController.java
+++ src/main/java/com/as3j/messenger/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.as3j.messenger.controllers;
 
+import com.as3j.messenger.common.MyPasswordEncoder;
+import com.as3j.messenger.dto.AddUserDto;
 import com.as3j.messenger.dto.EditUserDto;
 import com.as3j.messenger.exceptions.NoSuchFileException;
 import com.as3j.messenger.exceptions.NoSuchUserException;
+import com.as3j.messenger.exceptions.UserWithSuchEmailExistException;
 import com.as3j.messenger.model.entities.User;
 import com.as3j.messenger.services.FileService;
 import com.as3j.messenger.services.UserService;
@@ -20,11 +23,13 @@ public class UserController {
 
     private final UserService userService;
     private final FileService fileService;
+    private final MyPasswordEncoder encoder;
 
     @Autowired
-    public UserController(UserService userService, FileService fileService) {
+    public UserController(UserService userService, FileService fileService, MyPasswordEncoder encoder) {
         this.userService = userService;
         this.fileService = fileService;
+        this.encoder = encoder;
     }
 
     @PatchMapping
@@ -37,5 +42,19 @@ public class UserController {
             fileService.updatePhoto(editUserDto.getPhotoID().get(), user.getUuid());
         }
         userService.update(user);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public void registerUser(@RequestBody @Valid AddUserDto addUserDto) throws UserWithSuchEmailExistException {
+        userService.create(convertToUser(addUserDto));
+    }
+
+    private User convertToUser(AddUserDto addUserDto) {
+        User user = new User();
+        user.setEmail(addUserDto.getEmail());
+        user.setUsername(addUserDto.getUsername());
+        user.setPassword(encoder.encode(addUserDto.getPassword()));
+        return user;
     }
 }
