ea8b84a454d236df087be8b9bfd2381d7e440fd4
Jakub Zygmunt <216938@edu.p.lodz.pl>
2020-06-15 13:25:54 +0200

    add CORS methods

--- src/main/java/com/as3j/messenger/config/WebSecurityConfig.java
+++ src/main/java/com/as3j/messenger/config/WebSecurityConfig.java
@@ -81,7 +81,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
 
         configuration.addAllowedOrigin("*");
