84a74cfed3b5c925844ea8a8ae8a36c1420ab470
Jakub Zygmunt <216938@edu.p.lodz.pl>
2020-06-16 19:07:08 +0200

    Updates

---
+++ precache-manifest.f52f3a1feb402fca71f313fa5f763c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43e949c84e41071446d6b96e637594d",
+    "url": "/AS3J-Messenger/index.html"
+  },
+  {
+    "revision": "bb1fade4ed383b79f981",
+    "url": "/AS3J-Messenger/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e9c9c2565d0234ff5e4",
+    "url": "/AS3J-Messenger/static/js/2.9a0c8b3e.chunk.js"
+  },
+  {
+    "revision": "9acf1575b643fab8142f009bb8915834",
+    "url": "/AS3J-Messenger/static/js/2.9a0c8b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1fade4ed383b79f981",
+    "url": "/AS3J-Messenger/static/js/main.a81d9e68.chunk.js"
+  },
+  {
+    "revision": "401c47d9bc427c949624",
+    "url": "/AS3J-Messenger/static/js/runtime-main.c76401a5.js"
+  }
+]);
