465d11225da100d48d52d91165a0e008c9d21f68
216817 Kudaj Seweryn <216817@edu.p.lodz.pl>
2020-06-16 14:31:42 +0200

    Added curse wolrd filtration - language taken from request

--- src/main/java/com/as3j/messenger/controllers/MessageController.java
+++ src/main/java/com/as3j/messenger/controllers/MessageController.java
@@ -6,6 +6,7 @@ import com.as3j.messenger.exceptions.MessageAuthorIsNotMemberOfChatException;
 import com.as3j.messenger.exceptions.NoSuchChatException;
 import com.as3j.messenger.exceptions.NoSuchUserException;
 import com.as3j.messenger.model.entities.User;
+import com.as3j.messenger.profanity_filter.ProfanityFilterService;
 import com.as3j.messenger.services.MessageService;
 import com.as3j.messenger.services.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,6 +18,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.UUID;
 
 @RestController
@@ -26,6 +29,8 @@ public class MessageController {
     private final MessageService messageService;
     private final UserService userService;
     private final SimpMessagingTemplate webSocket;
+    @Autowired
+    private ProfanityFilterService profanityFilterService;
 
     @Autowired
     public MessageController(MessageService messageService, UserService userService, SimpMessagingTemplate webSocket) {
@@ -37,10 +42,12 @@ public class MessageController {
     @PostMapping(consumes = "application/json")
     public void sendMessage(@PathVariable("id") UUID chatUuid,
                             @AuthenticationPrincipal UserDetails userDetails,
-                            @RequestBody @Valid SingleValueDto<String> content) throws NoSuchUserException, NoSuchChatException,
-            MessageAuthorIsNotMemberOfChatException {
+                            @RequestBody @Valid SingleValueDto<String> content,
+                            Locale locale) throws NoSuchUserException, NoSuchChatException,
+            MessageAuthorIsNotMemberOfChatException, IOException {
         User author = userService.getByEmail(userDetails.getUsername());
-        MessageDto sentMessage = messageService.sendMessage(chatUuid, author, content.getValue());
+        String message = profanityFilterService.filterCurseWords(content.getValue(), locale.getLanguage());
+        MessageDto sentMessage = messageService.sendMessage(chatUuid, author, message);
 
         String destination = "/messages/add/" + chatUuid.toString();
         String payload = null;
--- src/test/java/controllers/MessageControllerTest.java
+++ src/test/java/controllers/MessageControllerTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.IOException;
+import java.util.Locale;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -46,12 +48,12 @@ public class MessageControllerTest {
     }
 
     @Test
-    void shouldAddMessage() throws NoSuchUserException, MessageAuthorIsNotMemberOfChatException, NoSuchChatException {
+    void shouldAddMessage() throws NoSuchUserException, MessageAuthorIsNotMemberOfChatException, NoSuchChatException, IOException {
         // given
         doReturn(author.getEmail()).when(userDetails).getUsername();
         doReturn(author).when(userService).getByEmail(any(String.class));
         // when
-        messageController.sendMessage(UUID.randomUUID(), userDetails, content);
+        messageController.sendMessage(UUID.randomUUID(), userDetails, content, Locale.ENGLISH);
         // then
         verify(userService, times(1)).getByEmail(any(String.class));
         verify(messageService, times(1)).sendMessage(any(UUID.class), any(User.class), any(String.class));
@@ -63,7 +65,7 @@ public class MessageControllerTest {
         doReturn(author.getEmail()).when(userDetails).getUsername();
         doThrow(NoSuchUserException.class).when(userService).getByEmail(any(String.class));
         // then
-        assertThrows(NoSuchUserException.class, () -> messageController.sendMessage(chatUuid, userDetails, content));
+        assertThrows(NoSuchUserException.class, () -> messageController.sendMessage(chatUuid, userDetails, content, Locale.ENGLISH));
     }
 
     @Test
@@ -73,6 +75,6 @@ public class MessageControllerTest {
         doReturn(author).when(userService).getByEmail(any(String.class));
         doThrow(NoSuchChatException.class).when(messageService).sendMessage(any(UUID.class), any(User.class), any(String.class));
         // then
-        assertThrows(NoSuchChatException.class, () -> messageController.sendMessage(chatUuid, userDetails, content));
+        assertThrows(NoSuchChatException.class, () -> messageController.sendMessage(chatUuid, userDetails, content, Locale.ENGLISH));
     }
 }
